1268103ddb2aa3762a6d6da19849f9d0e93dde09
Rob Ballantyne <rob@dynamedia.uk>
2021-09-17 15:49:45 +0100

    example postbody templates and localisation

---
+++ lang/en/lang.php
@@ -0,0 +1,48 @@
+<?php return [
+    'postbody_templates' => [
+        'recipe' => [
+            'name' => "Recipe Template",
+
+            'tabs' => [
+                'prologue'              => 'Prologue',
+                'detail'                => 'Detail',
+                'ingredients'           => 'Ingredients',
+                'ingredients_list'      => 'Ingredients List',
+                'ingredients_image'     => 'Ingredients Image',
+                'method'                => 'Method',
+                'epilogue'              => 'Epilogue',
+            ],
+
+            'labels' => [
+                'prep_time'     => 'Prep Time',
+                'name'          => 'Name',
+                'amount'        => 'Amount',
+                'step_details'  => "Step Details"
+            ]
+        ],
+        'product_review' => [
+            'name' => "Product Review",
+
+            'tabs' => [
+                'detail'        => 'Detail',
+                'introduction'  => 'Introduction',
+                'images'        => 'Images',
+                'positives'     => 'Positives',
+                'negatives'     => 'Negatives',
+                'verdict'       => 'Verdict',
+            ],
+
+            'labels' => [
+                'product_name'           => 'Product Name',
+                'product_buy_store'      => 'Where to Buy',
+                'product_buy_url'        => 'Buy URL',
+                'product_rating'         => 'Rating',
+                'reason'                 => 'Reason',
+                'image'                  => 'Image',
+                'alt_text'               => 'Alt Text',
+                'caption'                => 'Caption',
+
+            ]
+        ]
+    ]
+];
