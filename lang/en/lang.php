aa68d68e09665d0b21a2368524736b62347da67a
Rob Ballantyne <rob@dynamedia.uk>
2021-09-29 11:57:59 +0100

    schema for templates

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -6,6 +6,7 @@
             'tabs' => [
                 'prologue'              => 'Prologue',
                 'detail'                => 'Detail',
+                'timing'                => 'Timing',
                 'ingredients'           => 'Ingredients',
                 'ingredients_list'      => 'Ingredients List',
                 'ingredients_image'     => 'Ingredients Image',
@@ -14,10 +15,24 @@
             ],
 
             'labels' => [
-                'prep_time'     => 'Prep Time',
-                'name'          => 'Name',
-                'amount'        => 'Amount',
-                'step_details'  => "Step Details"
+                'category'              => 'Recipe Category',
+                'cuisine'               => 'Recipe Cuisine',
+                'servings'              => 'Number of Servings',
+                'serving_calories'      => 'Calories per Serving',
+                'prep_time_hours'       => 'Prep Time Hours',
+                'prep_time_minutes'     => 'Prep Time Minutes',
+                'cook_time_hours'       => 'Cooking Time Hours',
+                'cook_time_minutes'     => 'Cooking Time Minutes',
+                'total_time_hours'      => 'Total Time Hours',
+                'total_time_minutes'    => 'Total Time Minutes',
+                'image'                 => 'Image',
+                'name'                  => 'Name',
+                'amount'                => 'Amount',
+                'step_title'            => "Step Title",
+                'step_detail'           => "Step Detail"
+            ],
+            'comments' => [
+                'total_time'    => "Prep & cooking time plus extra (start to serving)"
             ]
         ],
         'product_review' => [
@@ -35,6 +50,8 @@
 
             'labels' => [
                 'product_name'           => 'Product Name',
+                'product_description'    => 'Product Description',
+                'product_info_url'       => 'Info URL',
                 'product_buy_store'      => 'Where to Buy',
                 'product_buy_url'        => 'Buy URL',
                 'product_rating'         => 'Rating',
@@ -42,7 +59,11 @@
                 'image'                  => 'Image',
                 'alt_text'               => 'Alt Text',
                 'caption'                => 'Caption',
-
+                'rating_1'               => '1 Star',
+                'rating_2'               => '2 Stars',
+                'rating_3'               => '3 Stars',
+                'rating_4'               => '4 Stars',
+                'rating_5'               => '5 Stars',
             ]
         ]
     ]
